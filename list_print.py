22e003953be14b82bdc2106095e1f4b3ef659cdd
HK <37285479+ultzikool@users.noreply.github.com>
2019-05-31 16:20:55 +0800

    Add files via upload
    move files

---
+++ list_print.py
@@ -0,0 +1,14 @@
+from list_print_function import print_lol
+
+cast = ["Palin","Cleese","Idle","Jones","Gilliam","Chapman"]
+
+movies = ["The Holy Grail",1975,"Terry Jones & Terry Gilliam",91,["Graham Chapman",["Michael Palin","John Cleese","Terry Gilliam","Eric Idle","Terry jones"]]]
+
+names = ["John","Eric",["cleese","Idle"],"Michael",["Palin"]]
+
+
+print_lol(names)
+
+print_lol(names,True)
+
+print_lol(names,True,4)
